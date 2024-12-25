7389fc63b844665cb1fc2d9db3bb67cb471111e4
xfnw <xfnw@riseup.net>
2024-12-25 10:50:47 -0500

    add syscall word
    replace moving between registers with xchg because its 1 byte smaller

--- vulpforth.asm
+++ vulpforth.asm
@@ -57,7 +57,7 @@ DEFWORD exit, 0b000, enter
 DEFWORD lit, 0b010, exit
 	push ebx	; make room in working stack
 	lodsw		; grab next colon-word token
-	mov ebx, eax	; put it on the stack
+	xchg ebx, eax	; put it on the stack
 	NEXT
 
 DEFWORD wpop, 'pop', 0b000, lit
@@ -72,7 +72,16 @@ DEFWORD nth, 0b000, dup
 	mov ebx, [esp+ebx]
 	NEXT
 
-DEFWORD plus, '+', 0b000, nth
+DEFWORD syscall, 0b000, nth
+	xchg eax, ebx	; get syscall number from top of stack
+	pop ebx		; syscall args
+	pop ecx
+	pop edx
+	int 0x80	; syscall
+	xchg ebx, eax	; put return value at our top of stack
+	NEXT
+
+DEFWORD plus, '+', 0b000, syscall
 	pop eax
 	add ebx, eax
 	NEXT

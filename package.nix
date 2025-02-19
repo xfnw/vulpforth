7119b27f9de06c25ab75e94ec75d31c20a8a2ea6
xfnw <xfnw@riseup.net>
2025-02-19 18:03:52 -0500

    double-sstrip zip output
    annoyingly upx creates some extra junk that sstrip can remove. it is
    only a hundred bytes but stripping it again now means that isolating the
    zip portion with zip -J, sstripping the binary to chop off the zip,
    concatenating them back together, and then correcting offsets with zip
    -A produces an identical binary

--- package.nix
+++ package.nix
@@ -27,6 +27,7 @@ stdenv_32bit.mkDerivation {
   '' + lib.optionalString (withTarget == "vulpforth.zip") ''
     ${perlPackages.strip-nondeterminism}/bin/strip-nondeterminism files.zip
     ${lib.optionalString withUpx "${upx}/bin/upx --best vulpforthzip"}
+    ${lib.optionalString withSstrip "${elfkickers}/bin/sstrip -z vulpforthzip"}
     make vulpforth.zip
   '';
 

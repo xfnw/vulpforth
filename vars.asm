775debf6cb054e35a41c1daa3c51bde9c0e8f86d
xfnw <xfnw@riseup.net>
2025-03-10 18:36:26 -0400

    rearrange vars

--- vars.asm
+++ vars.asm
@@ -4,16 +4,16 @@ DEFWORD hexchr, 0b01, 0
 DEFWORD wordfd, 0b01, hexchr
 	dd 0	; file descriptor to read words from
 
-DEFWORD here, 0b01, wordfd
+DEFWORD stackstart, 0b01, wordfd
+	dd 0	; address of start of working stack
+
+DEFWORD here, 0b01, stackstart
 	dd defhere	; address of next unused memory
 
 DEFWORD hereend, 0b01, here
 	dd retstack	; address of end of here bounds
 
-DEFWORD stackstart, 0b01, hereend
-	dd 0	; address of start of working stack
-
-DEFWORD latest, 0b01, stackstart
+DEFWORD latest, 0b01, hereend
 	dd init	; address of newest defined word
 
 DEFWORD loaded, 0b01, latest
